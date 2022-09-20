c48eb24494dbbc221aaef42ce5adbdca84bc5b2f
Nick Khoury <nkhoury@aeieng.com>
2022-09-19 18:29:24 -0700

    user icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from "react-router-dom";
+import { Avatar, Col, Layout, Row } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 import Inputs from "./pages/Inputs";
@@ -6,19 +9,34 @@ import Dashboard from "./pages/Dashboard";
 import Nav from "./components/Nav";
 import "./App.css";
 
+const { Header, Footer, Content } = Layout;
+
 const App = () => {
   return (
-    <>
-      <Nav />
-      <Routes>
-        <Route path="/">
-          <Route index element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/inputs" element={<Inputs />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </>
+    <Layout style={{ height: "100%" }}>
+      <Header>
+        <Row gutter={16}>
+          <Col>
+            <Nav />
+          </Col>
+          <Col flex="auto" />
+          <Col>
+            <Avatar icon={<UserOutlined />} />
+          </Col>
+        </Row>
+      </Header>
+      <Content style={{ height: "100%", padding: "2rem" }}>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Login />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/inputs" element={<Inputs />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>AEI 2022</Footer>
+    </Layout>
   );
 };
 
--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,15 +1,38 @@
-import { Col, Row } from "antd";
+import { Button, Col, Row } from "antd";
 import HubTree from "../components/HubTree";
 
 const Admin = () => {
   return (
-    <Row gutter={16}>
-      <Col span={8}>
-        <HubTree />
-      </Col>
-      <Col span={8}>two</Col>
-      <Col span={8}>three</Col>
-    </Row>
+    <>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Button
+            onClick={() => {
+              alert("todo");
+            }}
+          >
+            Add to be processed
+          </Button>
+        </Col>
+        <Col span={8}></Col>
+        <Col span={8}>
+          <Button
+            onClick={() => {
+              alert("todo");
+            }}
+          >
+            RUN
+          </Button>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={8}>
+          <HubTree />
+        </Col>
+        <Col span={8}>Models To Process</Col>
+        <Col span={8}>Process Log</Col>
+      </Row>
+    </>
   );
 };
 
