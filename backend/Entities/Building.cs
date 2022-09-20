3e81f13c7937a487f2badd21c2e37e4059e81166
Arif Hanif <arif.hanif3@gmail.com>
2022-09-20 17:53:52 -0400

    start work on design automation

--- backend/Entities/Building.cs
+++ backend/Entities/Building.cs
@@ -6,19 +6,19 @@ public class Building
     public Guid Id { get; set; }
     public string ProjectNumber { get; set; }
     public string Name { get; set; }
-    public string PrimaryBuildingType { get; set; }
-    public double Longitude { get; set; }
-    public double Latitude { get; set; }
+    public string? PrimaryBuildingType { get; set; }
+    public double? Longitude { get; set; }
+    public double? Latitude { get; set; }
 
     // Geometry
-    public double ExtractedFloorArea { get; set; }
+    public double? ExtractedFloorArea { get; set; }
     public double? FloorAreaOverride { get; set; }
-    public double ExtractedExteriorWallArea { get; set; }
+    public double? ExtractedExteriorWallArea { get; set; }
     public double? ExteriorWallAreaOverride { get; set; }
-    public double ExtractedExteriorGlazingArea { get; set; }
+    public double? ExtractedExteriorGlazingArea { get; set; }
     public double? ExteriorGlazingAreaOverride { get; set; }
-    public int NumberOfFloors { get; set; }
-    public double Height { get; set; }
+    public int? NumberOfFloors { get; set; }
+    public double? Height { get; set; }
     
     // Program
     public List<Model> Models { get; set; }
@@ -26,19 +26,19 @@ public class Building
     public List<RoomTypePercentage> RoomTypePercentages { get; set; }
     
     // Envelope
-    public double ExteriorWallUValue { get; set; }
-    public double GlazingUValue { get; set; }
-    public double GlazingSolarHeatGainCoefficient { get; set; }
-    public double InfiltrationRate { get; set; }
+    public double? ExteriorWallUValue { get; set; }
+    public double? GlazingUValue { get; set; }
+    public double? GlazingSolarHeatGainCoefficient { get; set; }
+    public double? InfiltrationRate { get; set; }
 
     // Operational Carbon
-    public double OperationalEnergyUseIntensity { get; set; } // kBtu/ ft^2 / yr
-    public double NaturalGasCarbonIntensity { get; set; } // kg CO2e / MBtu
-    public double NaturalGasEnergySourcePercentage { get; set; } 
-    public double ElectricityCarbonIntensity { get; set; } // lb CO2e / MWh
-    public double ElectricityEnergySourcePercentage { get; set; }
-    public double OtherEnergySourceCarbonIntensity { get; set; } // kg CO2e / kWh
-    public double OtherEnergySourcePercentage { get; set; }
+    public double? OperationalEnergyUseIntensity { get; set; } // kBtu/ ft^2 / yr
+    public double? NaturalGasCarbonIntensity { get; set; } // kg CO2e / MBtu
+    public double? NaturalGasEnergySourcePercentage { get; set; } 
+    public double? ElectricityCarbonIntensity { get; set; } // lb CO2e / MWh
+    public double? ElectricityEnergySourcePercentage { get; set; }
+    public double? OtherEnergySourceCarbonIntensity { get; set; } // kg CO2e / kWh
+    public double? OtherEnergySourcePercentage { get; set; }
 
     
     // Embodied
--- backend/Program.cs
+++ backend/Program.cs
@@ -3,6 +3,12 @@ using Backend.Entities;
 using Autodesk.Forge;
 using Microsoft.AspNetCore.Http.Json;
 using System.Text.Json.Serialization;
+using Autodesk.Forge.DesignAutomation.Model;
+using System.Net;
+using Autodesk.Forge.Model;
+using System;
+using Autodesk.Forge.Core;
+using Autodesk.Forge.DesignAutomation;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
@@ -46,7 +52,8 @@ app.MapGet("/models", async (BackendDbContext db) => await db.Models.ToListAsync
 app.MapPost("/add-model", async (ModelInput input, BackendDbContext db) =>
 {
     var model = new Model(input);
-    db.Models.Add(new Model(input));
+
+    db.Models.Add(model);
     await db.SaveChangesAsync();
     
     return Results.Created($"/models/{model.Id}", model);
@@ -105,6 +112,8 @@ app.MapDelete("/selected-activities/{id}", async (Guid activityId, BackendDbCont
 
 #region ExtractionLog
 
+
+
 app.MapGet("/extraction-log", async (BackendDbContext db) => await db.ExtractionLog.ToListAsync());
 app.MapPost("/run-activities", async (List<Guid> modelIds, BackendDbContext db) =>
 {
@@ -113,13 +122,92 @@ app.MapPost("/run-activities", async (List<Guid> modelIds, BackendDbContext db) 
         Id = Guid.NewGuid(),
     };
 
+    await db.ExtractionLog.AddAsync(extractionLog);
+
+    ForgeService service =
+                new ForgeService(
+                    new HttpClient(
+                        new ForgeHandler(Microsoft.Extensions.Options.Options.Create(new ForgeConfiguration()
+                        {
+                            ClientId = configuration["Forge:ClientId"],
+                            ClientSecret = configuration["Forge:ClientSecret"]
+                        }))
+                        {
+                            InnerHandler = new HttpClientHandler()
+                        })
+                );
+    var designAutomationClient = new DesignAutomationClient(service);
+
+    var ActivityFullName = "";
+
+    VersionsApi versionApi = new VersionsApi();
+    versionApi.Configuration.AccessToken = userAccessToken;
+    dynamic version = await versionApi.GetVersionAsync(projectId, versionId);
+    dynamic versionItem = await versionApi.GetVersionItemAsync(projectId, versionId);
+
+    string[] versionItemParams = ((string)version.data.relationships.storage.data.id).Split('/');
+    string[] bucketKeyParams = versionItemParams[versionItemParams.Length - 2].Split(':');
+    string bucketKey = bucketKeyParams[bucketKeyParams.Length - 1];
+    string objectName = versionItemParams[versionItemParams.Length - 1];
+    string downloadUrl = string.Format("https://developer.api.autodesk.com/oss/v2/buckets/{0}/objects/{1}", bucketKey, objectName);
+
+    var auth = await new TwoLeggedApi().AuthenticateAsync(
+        configuration["Forge:ClientId"],
+        configuration["Forge:ClientSecret"],
+        "client_credentials", new Scope[] { Scope.DataWrite, Scope.DataRead, Scope.BucketCreate, Scope.BucketRead });
+
+    var downloadURL = new XrefTreeArgument()
+    {
+        Url = string.Format("https://developer.api.autodesk.com/oss/v2/buckets/{0}/objects/{1}", bucketKey, objectName),
+        Verb = Verb.Get,
+        Headers = new Dictionary<string, string>()
+                {
+                    { "Authorization", "Bearer " + auth.access_token }
+                }
+    };
+
+    string bucketName = "revitdesigncheck" + NickName.ToLower();
+    BucketsApi buckets = new BucketsApi();
+    //dynamic token = await Credentials.Get2LeggedTokenAsync(new Scope[] { Scope.BucketCreate, Scope.DataWrite });
+    buckets.Configuration.AccessToken = auth.access_token;
+    PostBucketsPayload bucketPayload = new PostBucketsPayload(bucketName, null, PostBucketsPayload.PolicyKeyEnum.Transient);
+    try
+    {
+        await buckets.CreateBucketAsync(bucketPayload, "US");
+    }
+    catch { }
+
+    ObjectsApi objects = new ObjectsApi();
+    dynamic signedUrl = await objects.CreateSignedResourceAsyncWithHttpInfo(bucketName, resultFilename, new PostBucketsSigned(5), "readwrite");
+
+    var uploadUrl = new XrefTreeArgument()
+    {
+        Url = (string)(signedUrl.Data.signedUrl),
+        Verb = Verb.Put
+    };
+
+    string callbackUrl = string.Format("{0}/api/forge/callback/designautomation/{1}/{2}/{3}/{4}", Credentials.GetAppSetting("FORGE_WEBHOOK_URL"), userId, hubId, projectId, versionId.Base64Encode());
+
+    WorkItem workItemSpec = new WorkItem()
+    {
+        ActivityId = ActivityFullName,
+        Arguments = new Dictionary<string, IArgument>()
+        {
+            { "inputFile", await BuildDownloadURL(credentials.TokenInternal, projectId, versionId) },
+            { "result",  await BuildUploadURL(resultFilename) },
+            { "onComplete", new XrefTreeArgument { Verb = Verb.Post, Url = callbackUrl } }
+        }
+    };
+
     // Download Revit file from ACC 
     // Use data management bucket created by Eric upload model {Guid}.rvt,  inputParams : { "Operation": "Mechanical" } Rvt Version: 2022
     // Queue up design automation
     // Create web hooks or poll if runing out of time
     // Register call backs
 
-    await db.ExtractionLog.AddAsync(extractionLog);
+    WorkItemStatus workItemStatus = await designAutomationClient.CreateWorkItemAsync(workItemSpec);
+
+    
 });
 
 #endregion
