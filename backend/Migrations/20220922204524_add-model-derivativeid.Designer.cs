8fa9d053c83debf3612f2f3e9e10230b259a8106
Eric Berridge <eberridge@aeieng.com>
2022-09-22 13:50:08 -0700

    Add DerivativeId to the Model object and get migration up to par

--- backend/Migrations/20220922204524_add-model-derivativeid.Designer.cs
+++ backend/Migrations/20220922204524_add-model-derivativeid.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(BackendDbContext))]
-    partial class BackendDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220922204524_add-model-derivativeid")]
+    partial class addmodelderivativeid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
